e837d948aa9a2e06ad22fb453e6f241581fc2a2d
LesleyMdCampo <lesliz32@gmail.com>
2013-12-02 14:01:05 -0600

    Edit to html.rb main page for style purposes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 MetubeCohort3::Application.routes.draw do
 
-  get '/movies/gladiator' => 'movies#show_gladiator'
+  get '/movies/gladiator' => 'movies#show_gladiator', as: 'show_gladiator'
   get '/movies/matilda' => 'movies#show_matilda'
   get '/movies/sound_of_music' => 'movies#show_sound_of_music'
   get '/movies/shawshank_redemption' => 'movies#show_shawshank_redemption'
