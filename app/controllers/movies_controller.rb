8a77b6cb98f11dbbcc809a5351bc375d457c2bb8
LesleyMdCampo <lesliz32@gmail.com>
2013-12-02 10:52:30 -0600

    Initial commit, added two movies in routes, controllers and views.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,9 +14,27 @@ class MoviesController < ApplicationController
       title: 'Matilda',
       subtitle: "Big kid eating a brownie",
       video_id: 'EVWOQwZENBg',
-      description: "Quite the random movie, but a classic for sure!"
+      description: "Matilda does magic, just like rails."
     }
-    
   end
+
+  def show_sound_of_music
+    @movie = {
+      title: 'Sound of Music',
+      subtitle: "The Hills are Alive",
+      video_id: 'wbQSAdU4Qb4',
+      description: "Somewhere out there is a lady who I think will never be a nun. Auf Wiedersehen, darling."
+    }
+  end
+
+  def show_shawshank_redemption
+    @movie = {
+      title: 'Shawshank Redemption',
+      subtitle: "Red stands up to the system",
+      video_id: 'KtwXlIwozog',
+      description: "Remember Red, hope is a good thing, maybe the best of things, and no good thing ever dies."
+    }
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ MetubeCohort3::Application.routes.draw do
 
   get '/movies/gladiator' => 'movies#show_gladiator'
   get '/movies/matilda' => 'movies#show_matilda'
+  get '/movies/sound_of_music' => 'movies#show_sound_of_music'
+  get '/movies/shawshank_redemption' => 'movies#show_shawshank_redemption'
   
 end
