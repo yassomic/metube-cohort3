cf88630ed925444a271fd14bbb11dd1198bda63d
MakerSquare Student <students@themakersquare.com>
2013-12-04 12:14:41 -0600

    Added create, edit and destroy methods

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -19,9 +19,25 @@ class VideosController < ApplicationController
   	redirect_to root_path
   end
 
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    @video.update(video_params)
+    redirect_to root_path
+  end
+
   private
   def video_params
   	params.require(:video).permit(:title, :description, :youtube_id)
+  end
+
+  def destroy
+    @video = Video.find(params[:id])
+    @video.destroy
+    redirect_to root_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 MetubeCohort3::Application.routes.draw do
 
+  # root 'videos#show_all'
+  # get '/videos' => 'videos#show_all'
+  # get '/videos/new' => 'videos#new', as: 'new_video'
+  # get '/videos/:id' => 'videos#show', as: 'video'
+  # get '/videos/:id/edit' => 'videos#edit', as: 'edit_video'
+  # post '/videos' => 'videos#create'
+  # put '/videos/:id' => 'videos#update'
+  # patch '/videos/:id' => 'videos#update'
+  # delete '/videos/:id' => 'videos#destroy'
+
   root 'videos#show_all'
-  get '/videos' => 'videos#show_all'
-  get '/videos/new' => 'videos#new', as: 'new_video'
-  get '/videos/:id' => 'videos#show', as: 'video'
-
-  post '/videos' => 'videos#create'
-
+  resources :videos
 
 end
+
